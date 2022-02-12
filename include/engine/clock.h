903700b882fd4605cd19bb922e74ba80ea3ff3fb
Sevothart <santos.lucasmmatheus@gmail.com>
2022-02-12 10:32:29 -0300

    Clock class update
    - Documentation
    - New function names, needs to update other classes

--- include/engine/clock.h
+++ include/engine/clock.h
@@ -7,44 +7,39 @@
 class Clock
 {
 public:
-    /*! @brief
-        It's called on the start of the game loop, right before the event handling.
-    Get the last frame duration, setting internal dt parameters to the clock return.
+    /*!
+    @brief It's called at the start of the game loop, right before the event handling.
+    Gets the last frame duration and sets internal dt parameters to the clock return.
     */
-    void updateDt();
+    void updateDeltaTime();
 
-    /*! @brief
-        Called in every iteration of gameLoop, increasing the game elapsed time
+    /*!
+    @brief Called in every iteration of gameLoop, increasing the game elapsed time
     by the dt amount (current frame duration). Should be called after long operations
     and event handling.
     */
     void updateElapsed();
 
-    /*! @brief
-        Can be useful when showing time passed playing or when saving game files.
-    @return
-        Returns time passed since the game start.
+    /*!
+    @brief Can be useful when showing time passed playing or when saving game files.
+    @return Returns time passed since the game start.
     */
-    sf::Time& getElapsed();
+    sf::Time& elapsedTime();
 
-    /*! @brief
-        Very useful to be passed to update methods on entities.
-    @return
-        Returns time passed since the last frame start.
+    /*!
+    @brief Very useful to be passed to update methods on entities.
+    @return  Returns time passed since the last frame start.
     */
-    sf::Time& getDT();
+    sf::Time& deltaTime();
 
 private:
-    /*! @property
-    Used to get the last frame duration. */
+    /*! @brief sfml clock used for time measurement. */
     sf::Clock _clock;
 
-    /*! @property
-    Measure time passed since game start. */
+    /*! @brief Measure time passed since game start. */
     sf::Time _elapsed;
 
-    /*! @property
-    Stores last frame duration */
+    /*! @brief Stores last frame duration */
     sf::Time _dt;
 };
 
--- src/engine/clock.cpp
+++ src/engine/clock.cpp
@@ -1,6 +1,21 @@
 #include "engine/clock.h"
 
-void Clock::updateDt() { _dt = _clock.restart(); }
-void Clock::updateElapsed(){ _elapsed += _dt; }
-sf::Time& Clock::getElapsed(){ return _elapsed; }
-sf::Time& Clock::getDT() { return _dt; }
+void Clock::updateDeltaTime()
+{
+    _dt = _clock.restart();
+}
+
+void Clock::updateElapsed()
+{
+    _elapsed += _dt;
+}
+
+sf::Time& Clock::elapsedTime()
+{
+    return _elapsed;
+}
+
+sf::Time& Clock::deltaTime()
+{
+    return _dt;
+}
